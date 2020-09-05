be4eacf1413757d443bca0c41dbb45419b8cf083
Mohit Jain <jain.mohit27@gmail.com>
2020-09-06 01:40:15 +0530

    Moved action types to constants

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,44 +1,46 @@
+import * as actionTypes from './action';
+
 const initialState = {
     counter: 0,
     results: []
 }
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT'){
+    if(action.type === actionTypes.INCREMENT){
         return {
             ...state,
             counter: state.counter + 1
         }
     }
 
-    if(action.type === 'DECREMENT'){
+    if(action.type === actionTypes.DECREMENT){
         return {
             ...state,
             counter: state.counter - 1
         }
     }
 
-    if(action.type === 'ADD'){
+    if(action.type === actionTypes.ADD){
         return {
             ...state,
             counter: state.counter + action.val
         }
     }
 
-    if(action.type === 'SUBTRACT'){
+    if(action.type === actionTypes.SUBTRACT){
         return {
             ...state,
             counter: state.counter - action.val
         }
     }
 
-    if(action.type === 'STORE_RESULT'){
+    if(action.type === actionTypes.STORE_RESULT){
         return {
             ...state,
            results: state.results.concat({id: new Date(), value: state.counter})
         }
     }
 
-    if(action.type === 'DELETE_RESULT'){
+    if(action.type === actionTypes.DELETE_RESULT){
         const newArray = state.results.filter((result) => result.id !== action.resultElementId)
 
         return {
